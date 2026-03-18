4c5a589c32cec1ab81452d5d6e41ce0f2cfcb85f
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-18 20:56:10 -0300

    feat: desafio 1 - implementa classe Book com propriedades Title e Author.

---
+++ LibraryCatalog/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryCatalog
+{
+    public class Book
+    {
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+    }
+}
---
+++ LibraryCatalog/Program.cs
@@ -0,0 +1,14 @@
+﻿using LibraryCatalog;
+
+Console.Clear();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("                     CADASTRO DE LIVROS NA BIBLIOTECA");
+Console.ResetColor();
+Console.WriteLine();
+
+var myBook = new Book();
+myBook.Title = "Essencialismo";
+myBook.Author = "Greg Mckeown";
+
+Console.WriteLine($"  Livro: {myBook.Title} \n  Autor: {myBook.Author}");
