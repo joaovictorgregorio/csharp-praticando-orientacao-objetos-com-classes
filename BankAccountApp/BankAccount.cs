4e84ba154e3711e3a3826a1a7046e362a3f14fca
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-19 19:56:50 -0300

    feat: cria classe de conta bancária com depósito e exibição de saldo no console.
    - Adiciona classe BankAccount com número da conta e saldo
    - Implementa método Deposit para atualização do saldo
    - Exibe dados da conta e saldo formatado na execução do programa

---
+++ BankAccountApp/BankAccount.cs
@@ -0,0 +1,16 @@
+﻿namespace BankAccountApp
+{
+    public class BankAccount
+    {
+        public BankAccount(string? accountNumber, double balance)
+        {
+            AccountNumber = accountNumber;
+            Balance = balance;
+        }
+
+        public string? AccountNumber { get; set; }
+        public double Balance { get; set; }
+
+        public void Deposit(double value) => Balance += value;
+    }
+}
---
+++ BankAccountApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using BankAccountApp;
+
+Console.Clear();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("                     CONTA BANCÁRIA");
+Console.ResetColor();
+Console.WriteLine();
+
+var account = new BankAccount("6789675-3", 1200.00);
+account.Deposit(1200.00);
+
+Console.WriteLine($"  Conta: {account.AccountNumber} \n  Saldo atual: {account.Balance:C}");
