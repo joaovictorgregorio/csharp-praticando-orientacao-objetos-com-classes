8f5f9db7ef2fdfd736764dd203ca1a0b4f59e27d
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-19 20:49:24 -0300

    feat: implementa gerenciamento de funcionários com validação de promoção.
    - Adiciona classe Employee com nome e cargo
    - Implementa método Promote com validação para impedir promoção para o mesmo cargo
    - Exibe mensagens de erro e sucesso no console
    - Configura fluxo inicial no Program para simular promoções

---
+++ EmployeeManagementApp/Employee.cs
@@ -0,0 +1,38 @@
+﻿namespace EmployeeManagementApp
+{
+    public class Employee
+    {
+        public Employee(string? name, string? position)
+        {
+            Name = name;
+            Position = position;
+        }
+
+        public string? Name { get; set; }
+        public string? Position { get; set; }
+
+        public void Promote(string newPosition)
+        {
+            if (Position == newPosition)
+            {
+                Thread.Sleep( 1000 );
+                Console.WriteLine($"  Erro: O novo cargo deve ser diferente do cargo atual. \n  Cargo atual: {Position}");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Thread.Sleep( 1000 );
+                Console.WriteLine("\n  Promoção realizada com sucesso!  \n");
+                Console.ResetColor();
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Thread.Sleep( 1000 );
+                Console.WriteLine("  --- Após promoção ---  \n");
+                Console.ResetColor();
+
+                Console.WriteLine($"  Funcionário: {Name} \n  Cargo atual: {newPosition}");
+            }
+                
+        }
+    }
+}
---
+++ EmployeeManagementApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagementApp;
+
+Console.Clear();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.WriteLine("                     GERENCIAMENTO DE FUNCIONÁRIOS");
+Console.ResetColor();
+Console.WriteLine();
+
+
+var employee = new Employee("João Silva", "Estagiário");
+employee.Promote("Estagiário");
+employee.Promote("Analista");
