a4d4c20f39d041b1898ce891faabbdd61128a1ab
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-18 22:32:58 -0300

    feat: desafio 2 - implementa emissão de passagem com classe Ticket e exibição no console.

---
+++ TravelTicketSystem/Program.cs
@@ -0,0 +1,11 @@
+﻿using TravelTicketSystem;
+
+Console.Clear();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("                     EMISSÃO DE PASSAGEM DE VIAGEM");
+Console.ResetColor();
+Console.WriteLine();
+
+var ticket = new Ticket("João Victor", "Chile");
+Console.WriteLine($"  Passageiro: {ticket.Passenger} \n  Destino: {ticket.Destination}");
---
+++ TravelTicketSystem/Ticket.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelTicketSystem
+{
+    public class Ticket
+    {
+        public Ticket(string? passenger, string? destination)
+        {
+            Passenger = passenger;
+            Destination = destination;
+        }
+
+        public string? Passenger { get; set; }
+        public string? Destination { get; set; }
+    }
+}
