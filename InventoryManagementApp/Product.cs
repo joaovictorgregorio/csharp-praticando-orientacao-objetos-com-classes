28e42acbbff23ae6578306853cdf2528d9662c42
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-21 20:45:26 -0300

    feat: implementa controle de estoque com validação de retirada.
    - Adiciona classe Product com Name e estoque privado
    - Implementa método ToRemove com validação de quantidade disponível
    - Implementa DisplayInventory para exibir status do estoque
    - Configura fluxo no Program para testar cenários de retirada bem-sucedida e insuficiência

---
+++ InventoryManagementApp/Product.cs
@@ -0,0 +1,29 @@
+﻿namespace InventoryManagementApp;
+
+public class Product
+{
+    public Product(int inicialQuantity, string? name)
+    {
+        _stockQuantity = inicialQuantity;
+        Name = name;
+    }
+
+    private int _stockQuantity { get; set; }
+    public string? Name { get; set; }
+
+    public void ToRemove(int quantity) 
+    {
+        if (_stockQuantity >= quantity)
+        {
+            _stockQuantity -= quantity;
+
+            Console.WriteLine($"  Retirada de {quantity} unidades realizada com sucesso.");
+        }
+        else
+        {
+            Console.WriteLine($"  Erro: Estoque insuficiente para retirada de {quantity} unidades.");
+        }
+    }
+
+    public string DisplayInventory() => $" \n  Produto: {Name} \n  Estoque:{_stockQuantity}";
+}
---
+++ InventoryManagementApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using InventoryManagementApp;
+
+Console.Clear();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("                     CONTROLE DE ESTOQUE");
+Console.ResetColor();
+Console.WriteLine();
+
+var product = new Product(20, "Caneta Azul");
+product.ToRemove(5);
+product.DisplayInventory();
+product.ToRemove(30);
