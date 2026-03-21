e600999c2a01c2a2ed20ae50ab9ed9bbf09e2b3c
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-21 19:34:26 -0300

    feat: implementa classe Rectangle com cálculo de área no console.
    - Adiciona classe Rectangle com propriedades Height e Width
    - Implementa método CalculateArea para cálculo da área
    - Configura Program para instanciar retângulo e exibir resultado no console

---
+++ RectangleAreaCalculator/Program.cs
@@ -0,0 +1,11 @@
+﻿using RectangleAreaCalculator;
+
+Console.Clear();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("                     CALCULANDO ÁREA DE RETÂNGULOS");
+Console.ResetColor();
+Console.WriteLine();
+
+var rectangule = new Rectangle(5.0, 8.0);
+Console.WriteLine("  Área do Retângulo: " + rectangule.CalculateArea());
---
+++ RectangleAreaCalculator/Rectangle.cs
@@ -0,0 +1,15 @@
+﻿namespace RectangleAreaCalculator;
+
+public class Rectangle
+{
+    public Rectangle(double height, double width)
+    {
+        Height = height;
+        Width = width;
+    }
+
+    public double Height { get; set; }
+    public double Width { get; set; }
+
+    public double CalculateArea() => Height * Width;
+}
